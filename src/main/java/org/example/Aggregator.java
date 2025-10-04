0c9892ef7f8d53a28ce2e9944fffd98bb429c271
Anton Tsarenko <58445366+AntonTsa@users.noreply.github.com>
2025-10-04 14:28:53 +0300

    feat: Add feature2 (#2)
    Use squash to merge last commit to main

--- src/main/java/org/example/Aggregator.java
+++ src/main/java/org/example/Aggregator.java
@@ -1,11 +1,13 @@
 package org.example;
 
 import org.example.feature1.Feature1;
+import org.example.feature2.Feature2;
 
 public class Aggregator {
     private final Feature1 feature1 = new Feature1();
+    private final Feature2 feature2 = new Feature2();
 
-    public String getMainFeature() {
-        return feature1.getFeature1();
+    public String getFeatures() {
+        return feature1.getFeature1() + " & " + feature2.getFeature2();
     }
 }
---
+++ src/main/java/org/example/feature2/Feature2.java
@@ -0,0 +1,7 @@
+package org.example.feature2;
+
+public class Feature2 {
+    public String getFeature2() {
+        return "Feature 2";
+    }
+}
